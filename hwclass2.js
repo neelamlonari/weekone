f5ddebe723fff9c0c0226b30bfd5ad7c7fb2e532
neelamlonari <neelam.lonari@hotmail.com>
2022-12-06 22:40:59 -0500

    Adding Inheritence Class lab

---
+++ hwclass2.js
@@ -0,0 +1,26 @@
+
+   // There is only one Governor in the state, add static methods and properties you'd expect a Governor to have.
+   // log the properties and test the methods (don't instantiate)
+class Governor {
+    constructor (){
+    }
+    static name = 'Roy Cooper';
+    static state= 'North Carolina';
+
+    static getgovernordetail(){
+        return 'Roy Cooper,North Carolina';
+    }
+    
+    
+    static sayHello( hello){
+         return 'hello , from Trump';
+        
+    }
+
+}
+        console.log( Governor.sayHello());
+        console.log(Governor.getgovernordetail());
+        console.log(Governor.name);
+        console.log(Governor.state);
+ 
+  
